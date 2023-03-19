43cb62e18bc01095ecbefb394c5051036eec0e4d
Machfud Hidayat <machfudhidayat26@gmail.com>
2023-03-19 08:41:33 +0700

    Update with main

--- index.dart
+++ index.dart
@@ -1,7 +1,12 @@
 main() {
-  var phi = 3.14, r = 7;
+  var phi = 3.14, r = 14;
   var luasLingkaran = phi * r * r;
 
+  var phi2 = 3.14, r2 = 7;
+  var luasLingkaran2 = phi2 * r2 * r2;
+
   print('Hello World!');
-  print('Luas Lingkaran = $luasLingkaran');
+  print('Luas Lingkaran = $luasLingkaran2');
+  print('Luas Lingkaran =');
+  print(luasLingkaran.roundToDouble());
 }
